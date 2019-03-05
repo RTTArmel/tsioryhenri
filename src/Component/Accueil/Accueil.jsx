05d56f1b8160e03b3fe98f003659a2a5ac3a52ea
Henri <nambinintsoajeanyves@gmail.com>
2019-03-05 09:51:56 +0300

    envoier

---
+++ src/Component/Accueil/Accueil.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Accueil extends Component {
+  render() {
+    return (
+      <div>
+        <p>ce Vous êtes sur l'ACCUEIL</p>
+      </div>
+    );
+  };
+}
